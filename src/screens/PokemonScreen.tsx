c1dede51dc4089dc40a06abfc648458a0fe49134
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-05-22 20:04:08 -0300

    navigation and icons implemented

---
+++ src/screens/PokemonScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const PokemonScreen = () => {
+    return (
+        <View><Text>Pokemon Screen</Text></View>
+    )
+}
